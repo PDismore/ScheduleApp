512ede853fc53871fbb5107b93fe5426ac98af60
PDismore <pat.dismore@gmail.com>
2022-09-14 16:46:47 -0400

    js testing

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,10 +1,8 @@
-// Function to pull 
-var moment = require('moment');
-moment().format();
+
 function go (){
     let currentTime = moment().format("HH:mm");
 console.log(currentTime)
 }
+
+
 go;
-
-
