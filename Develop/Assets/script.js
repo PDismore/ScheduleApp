27b39e1d4e1321833f26c5338e97be7fc4c0a045
PDismore <pat.dismore@gmail.com>
2022-09-25 05:28:31 -0400

    created schedule template and save buttons

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,11 +1,33 @@
-var moment = require ('moment'); 
-moment().format();
+// var moment = require ('moment')
+// moment().format(); 
 
-function go (){
-    var currentTime = moment().format("HH:mm");
-console.log(currentTime)
+function go(){
+    var currentTime = 12;
+    for (var time =9; time<19; time++){
+        var id = time
+        var hourlyRow = $("<form>")
+            .addClass("row");
+        
+        $(".container").append(hourlyRow);
+
+        var timeBox = $("<div>")
+            .addClass("col-1 hour")
+            timeBox.text(time);
+            var contents = $('<textarea>')
+                .addClass('full col-10')
+                if (time > currentTime) contents.addClass('future')
+
+        var submitButton = $("<button> ")
+            .addClass("col-1 submitButton");
+            var saveImage = $('<i class="bi bi-save-fill"></i>')
+            submitButton.append(saveImage);
+
+        hourlyRow.append(timeBox, contents, submitButton);
+    console.log(id);
+
+    }
 }
-
-
-go;
-console.log(go);
+// document.getElementById('currentDay').textContent = moment().format('MMM Do YYYY, h:mm:ss a');
+// console.log(moment().format('MMM Do YYYY, h:mm:ss a'));
+go();
+// console.log(go);
